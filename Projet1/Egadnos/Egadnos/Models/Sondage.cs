dcdec1101c2b3a891fd61e6a2eff98d20d1729bb
Zammit <m@m.com>
2018-01-21 19:32:06 +0100

    Modification 3

--- Projet1/Egadnos/Egadnos/Models/Sondage.cs
+++ Projet1/Egadnos/Egadnos/Models/Sondage.cs
@@ -6,17 +6,17 @@ using System.Web.Mvc;
 
 namespace Egadnos.Models
 {
-    public class Sondage
+    class Sondage
     {
         public int NumSondage {get; set;}
         public string Titre {get; set;}
         public string Descriptif {get; set;}
         public bool ChoixMultiple {get; set;}
-        public string LienVote {get; set;}
+        public string LienVote {get; set; }
         public string LienSuppression {get; set;}
         public string LienConsultation {get; set;}
-        public bool SondageActif { get; set;}
-
-        
+        public bool SondageActif {get; set;}
     }
+
+    
 }
