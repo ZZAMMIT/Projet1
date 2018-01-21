d527712d15c4b3e1697ff0620646ef2eebf46d61
Zammit <m@m.com>
2018-01-21 15:13:07 +0100

    Premier Code

---
+++ Projet1/Egadnos/Egadnos/Controllers/CreationSondageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MonApplication.Models;
+using System.Data.SqlClient;
+
+namespace Egadnos.Controllers
+{
+    public class CreationSondageController : Controller
+    {
+        struct CreationSondage
+        {
+            public string TitreSondage;
+            public string DescriptifSondage;
+            //public TypeSondage Choix;
+        }
+
+        enum TypeSondage
+        {
+            ChoixMultiple,
+            ChoixUnique,
+        }
+        // GET: CreationSondage
+        public ActionResult Index()
+        {
+            CreationSondage sondage = new CreationSondage();
+            Console.WriteLine("Titre sondage");
+            sondage.TitreSondage = Console.ReadLine();
+            Console.WriteLine("Descrption sondage");
+            sondage.DescriptifSondage = Console.ReadLine();
+
+            Console.WriteLine("Type sondage: Multiréponse ? ");
+            string reponse = Console.ReadLine();
+   
+                
+
+            switch(reponse)
+            {
+                case "oui":
+                    Console.WriteLine("Vous avez choisi : " + TypeSondage.ChoixMultiple);
+                    break;
+                case "Non":
+                    Console.WriteLine("Vous avez choisi : " + TypeSondage.ChoixUnique);
+                    break;
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ Projet1/Egadnos/Egadnos/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using MonApplication.Models;
+
+/*/////////////////////////////////////////////////
+ * Page ok requete SQL a verifier 
+ * ///////////////////////////////////////////////*/
+
+
+namespace Egadnos.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            PageIndexModel model = new PageIndexModel();
+            model.CompteurVote = RecupererNombreVotesDepuisBDD();
+            return View(Model1);
+        }
+        
+        private const string SqlConnectionString =
+        @"Server=.\SQLExpress;Initial Catalog=Geandos; Trusted_Connection=Yes";
+
+        //Récupération du nombre de votes 
+        private int RecupererNombreVotesDepuisBDD()
+        {
+            SqlConnection connexion = new SqlConnection(SqlConnectionString);
+            connexion.Open();
+
+            SqlCommand recuperationNombreVotes =
+                new SqlCommand("SELECT COUNT(NumReponse) FROM ReponsePossible", connexion);
+
+            int nombreVotes = (int)recuperationNombreVotes.ExecuteScalar();
+
+            connexion.Close();
+
+            return nombreVotes;
+        }
+
+        //Récupération du score sondage
+        private int GetScoreSondageEnBDD()
+        {
+            SqlConnection connexion = new SqlConnection(SqlConnectionString);
+            connexion.Open();
+
+            SqlCommand recuperationScoreSondage =
+                new SqlCommand("SELECT Score FROM ReponsePossible", connexion);
+
+            int scoreSondage = (int)recuperationScoreSondage.ExecuteScalar();
+
+            connexion.Close();
+
+            return scoreSondage;
+        }
+
+        public ActionResult AutreChose()
+        {
+            return View();
+        }
+    }
+}
