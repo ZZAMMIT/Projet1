eee230eca0a4e2f4493a4b0995b4ff39c50120ad
MZammit <M@M.COM>
2018-01-31 10:08:14 +0100

    WebApplication

---
+++ Projet1/Egadnos/Egadnos/Models/Questionnaires.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Egadnos.Models
+{
+    public class Questionnaires
+    {
+        public string questionnaires { get; set; }
+        public string[] ReponseUtilisateur { get; set; }
+        
+    }
+}
